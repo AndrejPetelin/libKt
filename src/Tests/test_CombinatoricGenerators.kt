0bb5b048d749bc6449380a3535d34313d7038d08
Andrej Petelin <petelin.andrej@amis.net>
2017-08-23 18:31:39 +0200

    separate tests

---
+++ src/Tests/test_CombinatoricGenerators.kt
@@ -0,0 +1,30 @@
+/**
+ * Created by andrej on 23.8.2017
+ */
+
+
+
+import CombinatoricGenerators.combinations
+import CombinatoricGenerators.combinationsWithReplacement
+import CombinatoricGenerators.permutations
+import CombinatoricGenerators.permutationsWithReplacement
+
+fun main(args: Array<String>) {
+    val xs = permutationsWithReplacement("abc", 3)
+    xs.map { println(it) }
+
+    println()
+
+    val ys = permutations("abcd")
+    ys.map { println(it) }
+
+    println()
+
+    val zs = combinations("abcd", 2)
+    zs.map { println(it) }
+
+    println()
+
+    val ws = combinationsWithReplacement("abcd", 2)
+    ws.map { println(it) }
+}
