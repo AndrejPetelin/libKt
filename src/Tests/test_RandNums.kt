6ffeded63872696af4523a333b2e37fbf0140fe7
Andrej Petelin <petelin.andrej@amis.net>
2017-08-24 03:00:40 +0200

    added tests for random numbers lists

---
+++ src/Tests/test_RandNums.kt
@@ -0,0 +1,12 @@
+/**
+ * Created by andrej on 24.8.2017
+ */
+
+import RandNums.*
+
+fun main(args: Array<String>) {
+    println(randIntList(20))
+    println(randIntList(20, -50, 50))
+    println(randDoubleList(20))
+    println(randDoubleList(20, -50.0, 50.0))
+}
