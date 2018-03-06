68469add1aef793e5c4a49a2465eb30ecb16fe02
Andrej Petelin <petelin.andrej@amis.net>
2018-03-06 19:48:00 +0100

    enabled negative values, added Int overload

--- src/PowOfTwo/PowOfTwo.kt
+++ src/PowOfTwo/PowOfTwo.kt
@@ -1,12 +1,30 @@
 package PowOfTwo
 
+/**
+ * returns nearest lower power of two, note that lower means closer to 0,
+ * so nearestLowerPowOfTwo(-17) would be -16
+ */
+fun nearestLowerPowOfTwo(x: Int): Int {
+    var ret = 0;
+    var curr = 1;
+
+    while (curr <= Math.abs(x)) {
+        ret = curr;
+        curr = curr shl 1;
+    }
+    val sign = if (x < 0) 1 else -1;
+    return ret * sign;
+}
+
+
 fun nearestLowerPowOfTwo(x: Long): Long {
     var ret = 0L;
     var curr = 1L;
 
-    while (curr <= x) {
+    while (curr <= Math.abs(x)) {
         ret = curr;
         curr = curr shl 1;
     }
-    return ret;
+    val sign = if (x < 0) 1L else -1L;
+    return ret * sign;
 }
--- src/Tests/test_PowOfTwo.kt
+++ src/Tests/test_PowOfTwo.kt
@@ -3,7 +3,8 @@ package Tests
 import PowOfTwo.*
 
 fun main(args: Array<String>) {
+    println(nearestLowerPowOfTwo(0))
     println(nearestLowerPowOfTwo(16))
-    println(nearestLowerPowOfTwo(19))
+    println(nearestLowerPowOfTwo(-19L))
     println(nearestLowerPowOfTwo(132))
 }
